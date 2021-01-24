f0c885cff1b94c7c35e1f777e8f74d524d083df8
Roberto Eulogio Chavez Ramos <RobertoCodeCR@gmail.com>
2021-01-23 22:58:12 -0400

    Se quita comentarios de pruebas y espacios

--- verificarRepositoriosGitRECHR.sh
+++ verificarRepositoriosGitRECHR.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-
+#Roberto Eulogio Chavez Ramos (RobertoCodeCR, RobertoIngECR, RECHR)
 # Check system language and add strings
-case $LANG in 
+case $LANG in
     es_ES.UTF-8)
         check_modif="modificado"
         message_modif="modificado"
@@ -59,7 +59,7 @@ do
 	# Check if directory is a git repository
 	if [ -d "$f/.git" ]
 	then
-        mod=0	
+        mod=0
 		modificado=""
 		no_agregado=""
 		adelantado=""
@@ -105,23 +105,6 @@ do
 			echo -e " "
 			echo
 		fi
-
-		# Check if everything is peachy keen
-		# if [ $mod -eq 0 ]
-		# then
-		# 	echo -en "$clean"
-		# fi
-
-
-        
 		cd ../
-	# else
-    #     # Not a git repository
-
-	#     echo -en "\033[0;37m"
-    # 	echo "${f}"
-	# 	echo "$no_git"
-	#     echo -en "\033[0m"
 	fi
-    #echo
 done
